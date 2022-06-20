9124977cc0e2e4332185a279e3f3ef3e8c9db752
MiguelGuevara25 <miguel_g1789@hotmail.com>
2022-06-20 18:38:54 -0500

    change

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,13 @@ const {
     dest,
     watch
 } = require("gulp");
+
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done) {
-    src('src/scss/app.scss')
+    src('src/scss/**/*.scss')
+        .pipe(plumber())
         .pipe(sass())
         .pipe(dest("build/css"))
 
@@ -14,7 +17,7 @@ function css(done) {
 }
 
 function dev(done){
-    watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css);
 
     done();
 }
